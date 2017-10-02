a828958d3412f36f3140e631255623cb8cdb5b70
xiayto <574528252@qq.com>
2017-10-02 15:49:30 +0800

    Update twoSum.cpp

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,4 +1,18 @@
 class Solution{
+public:
+  vector<int> twoSum(vector<int>& nums,int target){
+    unorder_map<int,int> m;
+    vector<int> res;
+    for(int i=0;i<nums.size();i++){
+      if(m.count(target-nums[i])){
+        return {i,m[target-nums[i]]
+      };
+      m[nums[i]]=i;
+    }
+}
+  
+      
+  
 
 
 
