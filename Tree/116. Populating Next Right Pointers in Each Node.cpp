18cdde1efdde23e34554da01d4553611e92717e4
xiayto <574528252@qq.com>
2018-03-23 16:39:35 +0800

    用next指针层次遍历二叉树

--- Tree/116. Populating Next Right Pointers in Each Node.cpp
+++ Tree/116. Populating Next Right Pointers in Each Node.cpp
@@ -6,6 +6,8 @@
  *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
  * };
  */
+
+// 用队列层次遍历，不满足空间复杂度O(1)的要求
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
@@ -35,6 +37,27 @@ public:
             curLayerNum = nextLayerNum;
             i = 0;
             nextLayerNum = 0;
+        }
+    }
+};
+
+
+//利用next指针层次遍历
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if(root == nullptr)
+            return;
+        TreeLinkNode* cur;
+        while(root->left != nullptr){
+            cur = root;
+            while(cur != nullptr){
+                cur->left->next = cur->right;
+                if(cur->next != nullptr)
+                    cur->right->next = cur->next->left;
+                cur = cur->next;
+            }
+            root = root->left;
         }
     }
 };
