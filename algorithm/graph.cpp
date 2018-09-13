0f17f12b35e537bbe666c85fbc7960b01b0a3581
xiayto <574528252@qq.com>
2018-09-14 02:34:05 +0800

    Update graph.cpp

--- algorithm/graph.cpp
+++ algorithm/graph.cpp
@@ -5,6 +5,8 @@ struct GraphNode{
     GraphNode(int x):label(x){}
 };
 
+//-----------------------------------------------------------------------
+
 // 给数组赋予初值，memset函数 memset(数组名, value, n*sizeof(int))
 int a[50];
 memset(a, 0, 50*sizeof(int));
@@ -17,6 +19,57 @@ void DFS(GrpahNode *node, int visit[]){
         }
     }
 }
+
+// leetcode 207 变为考察图中有无环
+struct GraphNode{
+    int val;
+    vector<GraphNode*> neighbors;
+    GraphNode(int x):val(x){};
+};
+
+bool DFS(GraphNode* node, vector<int> &visit){
+    visit[node->val] = 0;
+    for(int i = 0; i < node->neighbors.size(); i++){
+        if(visit[node->neighbors[i]->val] == -1){
+            if(DFS(node->neighbors[i], visit) == 0){
+                return false;
+            }
+        }
+        else if(visit[node->neighbors[i]->val] == 0){
+            return false;
+        }
+    }
+    visit[node->val] = 1;
+    return true;
+}
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<int> visit;
+        vector<GraphNode*> graph;
+        for(int i = 0; i < numCourses; i++){
+            graph.push_back(new GraphNode(i));
+            visit.push_back(-1);
+        }
+        for(int i = 0; i < prerequisites.size(); i++){
+            GraphNode* end = graph[prerequisites[i].first];
+            GraphNode* begin = graph[prerequisites[i].second];
+            begin->neighbors.push_back(end);
+        }
+        for(int i = 0; i < graph.size(); i++){
+            if(!DFS(graph[i], visit) || visit[i] == -1)
+                return false;
+        }
+        for (int i = 0; i < numCourses; i++){
+            delete graph[i];
+        }
+        return true;
+    }
+};
+
+
+//-----------------------------------------------------------------------
 
 //广度优先搜索
 void BFS(GraphNode* node, int visit[]){
@@ -34,3 +87,10 @@ void BFS(GraphNode* node, int visit[]){
         }
     }
 }
+
+// leetcode 207 广度优先解法，入度为0的节点加入队列
+
+
+
+
+
