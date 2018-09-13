9a4b35f2632ba7eba84ca805d8ca6a685a1743b8
xiayto <574528252@qq.com>
2018-09-13 09:52:18 +0800

    Create stack_and_queue.cpp

---
+++ algorithm/stack_and_queue.cpp
@@ -0,0 +1,14 @@
+#include <stack> 
+S.top() //取出栈顶 
+S.empty() //判断栈是否为空 
+S.push(x) //将x压入栈中 
+S.pop() //弹出栈顶 
+S.size() //栈的存储元素个数
+
+#include <queue> 
+Q.empty() //判断队列是否为空 
+Q.front() //返回队列的头部元素 
+Q.back() //返回队列的尾部元素 
+Q.pop() //弹出队列头部元素 
+Q.push(x) //将x添加至队列 
+Q.size() //返回队列中的存储元素个数
